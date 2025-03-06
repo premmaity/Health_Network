72b97a13318e83ecd905f25fde9ffc55b7d2bf98
premmaity <112617129+premmaity@users.noreply.github.com>
2025-03-06 16:44:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -479,4 +479,4 @@ if __name__ == '__main__':
     with app.app_context():
         
         db.create_all()
-    socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", port=port, debug=True)
